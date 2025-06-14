c4dcc08860dc3bfa214b2d8de7dbda374a481a48
Pedro Figueira <pedrofigueiraboaviagem@gmail.com>
2025-06-14 00:04:12 -0300

    fix: simplified existing code

--- app/File/FileReader.hs
+++ app/File/FileReader.hs
@@ -3,12 +3,14 @@ module FileReader (fileRead) where
 import System.IO (IOMode (ReadMode), hGetLine, hIsEOF, openFile)
 import System.IO.Error (catchIOError)
 
--- reads the file fully and returns the combination of the annotations and functions (or a String error)
 fileRead :: String -> IO (Either String [(String, String)])
 fileRead path =
   catchIOError
-    (Right . findAnnotationsAndFunctions <$> readFileLines path) -- fmap over the findAnnotationsAndFunctions(lines)
-    (\e -> return . Left $ "Error reading file: " ++ show e)
+    (do
+      fileLines <- readFileLines path
+      return (Right (findAnnotationsAndFunctions fileLines))
+    ) -- no errors
+    (\e -> return (Left ("Error reading file: " ++ show e))) -- error string
 
 -- reads the file line by line and returns a list of lines
 readFileLines :: String -> IO [String]
@@ -39,19 +41,18 @@ findAnnotationsAndFunctions (currentLine : otherLines) =
         [] -> findAnnotationsAndFunctions otherLines
     Nothing -> findAnnotationsAndFunctions otherLines
 
--- parses the annotation from the line, extracting everything AFTER "qqc:"
+-- parses the annotation from the line, extracting everything after the "@quickcheck" annotation
 parseAnnotation :: String -> Maybe String
 parseAnnotation line =
-  let found = "@quickcheck"
-      ws = words line -- split the line into words
-   in case dropWhile (/= found) ws of
-        (_ : rest) | not (null rest) -> Just (unwords rest) -- if the found word is not the last one, return the rest of the line
-        _ -> Nothing
+  case dropWhile (/= "@quickcheck") (words line) of
+    foundWordAndRest ->
+      if length foundWordAndRest > 1
+        then Just (unwords (tail foundWordAndRest)) -- if "@quickcheck" is not the last word, return the rest
+        else Nothing
 
 -- checks if the line is a function definition
 isFunction :: String -> Bool
 isFunction line =
-  let wordsInLine = words line
-   in case wordsInLine of
-        (name : _ : _) -> not (null name) && "->" `elem` wordsInLine -- check if the line has a function name and an arrow
-        _ -> False -- not a function definition
+  case words line of
+    (name : second : rest) -> not (null name) && "->" `elem` (second : rest) -- checks the normal function definition (name args -> returnType)
+    _other -> False
